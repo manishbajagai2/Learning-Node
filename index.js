3ea34d5a8e3df780d42be1fe519017f74895a82c
manish <manishbajagai2@gmail.com>
2023-12-27 18:58:31 +0530

    fs module with sync and async nature

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-const buffer = new Buffer.from("Manish")
+const fs = require("node:fs")
 
-buffer.write("Code")  // rewriting the buffer replacing the original temporal character
+console.log("First")
 
-console.log(buffer)
-console.log(buffer.toString())
-console.log(buffer.toJSON())
+const fsReader = fs.readFileSync("./hello.txt", "utf-8")
+console.log(fsReader)
+console.log("Second")
+
+fs.readFile("./hello.txt", "utf-8", (err, data) => {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log(data)
+    }
+})
+
+console.log("Third")
+
+fs.writeFileSync("new.txt", "A new file")
+
+fs.writeFile("new.txt", " New content appended", { flag: "a" }, (err) => {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log("File written successfully")
+    }
+})
