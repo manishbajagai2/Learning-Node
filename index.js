2e694f8afcd683c36fd433cf7c9ab3fc62d13956
manish <manishbajagai2@gmail.com>
2023-12-27 19:07:08 +0530

    fs module using promises

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
-const fs = require("node:fs")
+const fs = require("node:fs/promises")
 
-console.log("First")
+// console.log('First')
 
-const fsReader = fs.readFileSync("./hello.txt", "utf-8")
-console.log(fsReader)
-console.log("Second")
+// fs.readFile("./hello.txt","utf-8")
+// .then(data => console.log(data))
+// .catch(err => console.log(err))
 
-fs.readFile("./hello.txt", "utf-8", (err, data) => {
-    if (err) {
-        console.log(err)
-    } else {
+// console.log('Second')
+
+async function readFileData() {
+    try {
+        const data = await fs.readFile("./hello.txt", "utf-8")
         console.log(data)
-    }
-})
-
-console.log("Third")
-
-fs.writeFileSync("new.txt", "A new file")
-
-fs.writeFile("new.txt", " New content appended", { flag: "a" }, (err) => {
-    if (err) {
+    } catch (err) {
         console.log(err)
-    } else {
-        console.log("File written successfully")
     }
-})
+}
+
+readFileData()
