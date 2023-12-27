7f08211b8fe11dd1d3137ec814b339b4b0291868
manish <manishbajagai2@gmail.com>
2023-12-27 20:49:41 +0530

    HTML Routing with Nodejs

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const http = require("node:http")
 const fs = require("node:fs")
 
-const server = http.createServer((req,res) => {
-    const name = "Manish"
-    res.writeHead(200, { "Content-Type": "text/html"})
-    let data = fs.readFileSync(__dirname + "/index.html", "utf-8")
-    data = data.replace("{{name}}",name)
-    res.end(data)
+const server = http.createServer((req, res) => {
+    if (req.url == "/") {
+        res.writeHead(200, { "Content-Type": "text/plain" })
+        res.end("Home Page")
+    } else if (req.url == "/about") {
+        res.writeHead(200)
+        res.end("About Page")
+    } else if (req.url == "/api") {
+        res.writeHead(200, { "Content-Type": "application/json" })
+        res.end(
+            JSON.stringify({
+                firstName: "Bruce",
+                lastName: "Wayne",
+            })
+        )
+    } else {
+        res.writeHead(404)
+        res.end("Page not Found !!!")
+    }
 })
 
 server.listen(3000, () => {
-    console.log('Server listening at port 3000')
+    console.log("Server listening at port 3000")
 })
