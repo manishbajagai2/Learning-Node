6b888f51292ebe7f8a21007bb63a713c58ce7c8c
manish <manishbajagai2@gmail.com>
2023-12-27 20:01:56 +0530

    Creating node server using http module

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
-// Pipes retuns the destination stream enabling chaining, condition is
-// the destination stream should be readable, duplex or a transform stream
-// In the example, we have writableStream at last so we cannot call pipe, so using zlib
+// The HTTP module allows the creation of web servers that can transfer over HTTP
 
-const fs = require("node:fs")
-const zlib = require("node:zlib") // Provides file compression using zlib algorithm
+const http = require("node:http")
 
-const gzip = zlib.createGzip() // will be a transform stream to enable pipe
-
-const readableStream = fs.createReadStream("./file1.txt", {
-    encoding: "utf-8",
-    highWaterMark: 2, // since default buffer size used by stream is 64kb
+const server = http.createServer((req, res) => {
+    // console.log(req)
+    res.writeHead(200, { "Content-Type": "text/plain" })
+    res.end("Responding Hello World")
 })
 
-// const writableStream = fs.createWriteStream("./file2.txt")
-
-// readableStream.on("data", (chunck) => {
-//     console.log(chunck)
-//     writableStream.write(chunck)
-// })
-
-// A simplier and better way is to use pipe
-
-// readableStream.pipe(writableStream)
-
-readableStream.pipe(gzip).pipe(fs.WriteStream("./file2.txt.gz"))
+server.listen(3000, () => {
+    console.log("Server listening on port 3000")
+})
