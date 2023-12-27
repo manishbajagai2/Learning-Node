579776da7fd0bf16604e7f1994c8fc8a33054f4b
manish <manishbajagai2@gmail.com>
2023-12-27 20:41:17 +0530

    Working with HTML Templates

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-// The HTTP module allows the creation of web servers that can transfer over HTTP
-
 const http = require("node:http")
 const fs = require("node:fs")
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/html" })
-
-    // const data = fs.readFileSync(__dirname + "/index.html", "utf-8") // This is reading the entire html content and increasing unnecessary buffer size
-    // res.end(data)
-
-    // So we will use readStream to save memory and time
-    fs.createReadStream(__dirname + "/index.html").pipe(res)
+const server = http.createServer((req,res) => {
+    const name = "Manish"
+    res.writeHead(200, { "Content-Type": "text/html"})
+    let data = fs.readFileSync(__dirname + "/index.html", "utf-8")
+    data = data.replace("{{name}}",name)
+    res.end(data)
 })
 
 server.listen(3000, () => {
-    console.log("Server listening on port 3000")
+    console.log('Server listening at port 3000')
 })
