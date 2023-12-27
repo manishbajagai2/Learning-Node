9c7b924c6a0d48b942c57457ec347b2a198062e1
manish <manishbajagai2@gmail.com>
2023-12-27 20:29:41 +0530

    Sending entire html file as response

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 // The HTTP module allows the creation of web servers that can transfer over HTTP
 
 const http = require("node:http")
+const fs = require("node:fs")
 
 const server = http.createServer((req, res) => {
-    // console.log(req)
-    const superHero = {
-        firstName: "Bruce",
-        lastName: "Wayne"
-    }
-    res.writeHead(200, { "Content-Type": "application/json" })
-    res.end(JSON.stringify(superHero))
+    res.writeHead(200, { "Content-Type": "text/html" })
+
+    // const data = fs.readFileSync(__dirname + "/index.html", "utf-8") // This is reading the entire html content and increasing unnecessary buffer size
+    // res.end(data)
+
+    // So we will use readStream to save memory and time
+    fs.createReadStream(__dirname + "/index.html").pipe(res)
 })
 
 server.listen(3000, () => {
